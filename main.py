3cd7435aed95ec108ee6f4b79fc0d6b1b1f9dee6
tihomirtx88 <tihomirtx88@gmail.com>
2025-07-22 09:38:58 +0300

    Add thumbnail titles
    Add thumbnail titles

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide");
 
@@ -13,3 +14,15 @@ with col2:
      Hi, I am Tihomir. I am Python and JavaScript programmer.
     """
     st.info(content)
+
+df = pandas.read_csv("data.csv", sep=";");
+col3, col4 = st.columns(2)
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"]);
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"]);
+
